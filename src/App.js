6207413790cf16e9810f7014d57e7b4e7b93e843
PawanKalyancn <119735090+PawanKalyancn@users.noreply.github.com>
2023-10-02 12:06:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import TodoApp from './TodoComponents/TodoApp';
+function App() {
+  return (
+    <div className="container d-flex justify-content-center bg-primary">
+  <TodoApp></TodoApp>
+
+   </div>
+  );
+}
+
+export default App;
